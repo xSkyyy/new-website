9536b89e465e3d629e49a6baccdc9e86356be955
Skylar <mc360masteryt@gmail.com>
2024-11-22 10:11:58 +0000

    attempt redirect

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import { Github, Twitter, Mail, PawPrint, Send } from 'lucide-react';
 import SocialLink from './components/SocialLink';
 import Avatar from './components/Avatar';
 import BubbleBackground from './components/BubbleBackground';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ButterflyRedirect from './ButterflyRedirect';
+
+
 
 function App() {
   return (
@@ -50,6 +54,20 @@ function App() {
       </div>
     </>
   );
+}
+
+const App: React.FC = () => {
+    return (
+        <Router>
+              <Routes>
+                      <Route path="/" element={<MainSite />} /> {/* Main Site Component */}
+                              <Route path="/butterfly" element={<ButterflyRedirect />} />
+                                      {/* Other routes */}
+                                            </Routes>
+                                                </Router>
+                                                  );
+                                                  };
+                                                  
 }
 
 export default App;
