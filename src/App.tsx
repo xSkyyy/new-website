178e1e29bcdfa7174b892c819af7e84dec4b0496
Skylar <mc360masteryt@gmail.com>
2024-11-22 09:38:18 +0000

    fix avatar

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
         <div className="relative z-10 min-h-screen flex items-center justify-center p-4">
           <div className="max-w-md w-full space-y-8 py-12">
             <Avatar
-              imageUrl="media/pfp.jpg"
+              imageUrl="public/media/pfp.jpg"
               name="SkylarFem"
               title="Content Creator // Developer // Administrator"
             />
@@ -26,7 +26,7 @@ function App() {
               />
               <SocialLink
                 icon={<Twitter className="w-5 h-5" />}
-                label="Twitter (Potentially depricated)"
+                label="Twitter (Depricated)"
                 href="https://x.com/skylarfem"
               />
               <SocialLink
